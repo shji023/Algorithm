c87e77000b8d10fd8818c0ca04b425d65e61101c
shji023 <shji0@naver.com>
2021-01-26 17:09:51 +0900

    21.01.26

---
+++ Programmers/네트워크.py
@@ -0,0 +1,22 @@
+from collections import deque
+
+
+def bfs(computers, i, visited,n):
+    visited[i] = True
+    queue = deque([i])
+    while queue:
+        v = queue.popleft()
+        visited[v] = True
+        for t in range(n):
+            if computers[v][t] == 1:
+                if visited[t] == False:
+                    queue.append(t)
+
+def solution(n, computers):
+    answer = 0
+    visited = [False] * n
+    for i in range(n):
+        if visited[i] == False:
+            bfs(computers, i, visited, n)
+            answer += 1
+    return answer
