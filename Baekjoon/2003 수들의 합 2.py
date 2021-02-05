d027f1bfd23e2b82e057820f9c44fa383c0f31ed
shji023 <shji0@naver.com>
2021-02-05 14:41:17 +0900

    21.02.05

---
+++ Baekjoon/2003 수들의 합 2.py
@@ -0,0 +1,14 @@
+N, M = list(map(int, input().split()))
+a = list(map(int, input().split()))
+cnt = 0
+interval_sum = 0
+end = 0
+
+for start in range(N):
+    while interval_sum < M and end < N:
+        interval_sum += a[end]
+        end += 1
+    if interval_sum == M:
+        cnt += 1
+    interval_sum -= a[start]
+print(cnt)
