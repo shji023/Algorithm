a2e48ca8b5e139bb806474331213e4fe7bb9c5f7
shji023 <shji0@naver.com>
2021-02-10 11:42:06 +0900

    21.02.10

---
+++ Baekjoon/7785 회사에 있는 사람.py
@@ -0,0 +1,16 @@
+# set 관련 함수 이용
+n = int(input())
+people = set([])
+arr = []
+for i in range(n):
+    log = input().split()
+    if log[1] == 'enter':
+        people.add(log[0])
+    else:
+        people.remove(log[0])
+
+for x in people:
+    arr.append(x)
+arr.sort(reverse=True)
+for y in arr:
+    print(y)
