af0f2964212160ddc00db76755c26a96c4a2b3dc
shji023 <shji0@naver.com>
2021-02-15 23:15:49 +0900

    21.02.15

---
+++ Baekjoon/2751 수 정렬하기2.py
@@ -0,0 +1,41 @@
+import sys
+input = sys.stdin.readline
+N = int(input())
+a = list(int(input())for _ in range(N))
+a.sort()
+for x in a:
+    print(x)
+    
+'''
+시간초과
+import sys
+input = sys.stdin.readline
+
+N = int(input())
+a = list(int(input()) for _ in range(N))
+
+
+def qsort(arr, start, end):
+    if start >= end:
+        return
+    pivot = start
+    left = start + 1
+    right = end
+    while left <= right:
+        while left <= end and arr[left] < arr[pivot]:
+            left += 1
+        while right > start and arr[right] >= arr[pivot]:
+            right -= 1
+        if left > right:
+            arr[right], arr[pivot] = arr[pivot], arr[right]
+        else:
+            arr[left], arr[right] = arr[right], arr[left]
+    qsort(arr, start, right - 1)
+    qsort(arr, right + 1, end)
+
+
+qsort(a, 0, N - 1)
+for x in a:
+    print(x)
+'''
+
