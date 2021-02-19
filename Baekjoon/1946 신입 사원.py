6171841de8c4cdb2ace746a98f70864ea6e20c64
shji023 <shji0@naver.com>
2021-02-19 18:04:37 +0900

    21.02.19

---
+++ Baekjoon/1946 신입 사원.py
@@ -0,0 +1,15 @@
+import sys
+input = sys.stdin.readline
+T = int(input())
+
+for _ in range(T):
+    N = int(input())
+    cnt = 1
+    arr = [list(map(int, input().split())) for _ in range(N)]
+    arr.sort(key=lambda x: x[0])
+    min_arr = arr[0][1]
+    for i in range(1, N):
+        if arr[i][1] < min_arr:
+            min_arr = arr[i][1]
+            cnt += 1
+    print(cnt)
