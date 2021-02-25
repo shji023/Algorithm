6e24c2c036409b2273f5ef178741e64744e8b4ec
shji023 <shji0@naver.com>
2021-02-25 21:41:50 +0900

    21.02.25

---
+++ Baekjoon/1697 숨바꼭질.py
@@ -0,0 +1,20 @@
+import sys
+from collections import deque
+input = sys.stdin.readline
+
+N, K = map(int, input().split())
+Max = 100001
+def bfs(start):
+    queue = deque([start])
+    while queue:
+        v = queue.popleft()
+        if v == K:
+            print(time[v])
+            return
+        for i in (v-1, v+1, 2*v):
+            if (0 <= i < Max) and time[i] == 0:
+                time[i] = time[v] + 1
+                queue.append(i)
+
+time = [0]*Max
+bfs(N)
