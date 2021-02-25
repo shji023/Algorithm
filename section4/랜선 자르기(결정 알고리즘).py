25bfddf3d26794617cad4769ee9ab64d3b2d7002
shji023 <shji0@naver.com>
2021-02-25 17:16:09 +0900

    21.02.25

---
+++ section4/랜선 자르기(결정 알고리즘).py
@@ -0,0 +1,19 @@
+import sys
+input = sys.stdin.readline
+K, N = map(int, input().split())
+arr = list(int(input()) for _ in range(K))
+
+start = 1
+end = max(arr)
+res = 0
+while start <= end:
+    total = 0
+    mid = (start+end)//2
+    for x in arr:
+        total += x//mid
+    if total >= N:
+        res = mid
+        start = mid + 1
+    else:
+        end = mid - 1
+print(res)
