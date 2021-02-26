06f8d3337beb9d91fc879ca3ed0b361deb665c4e
shji023 <shji0@naver.com>
2021-02-26 17:13:18 +0900

    SQL/SUM, MAX, MIN

---
+++ Programmers/SUM, MAX, MIN.sql
@@ -0,0 +1,25 @@
+/*최댓값 구하기*/
+SELECT MAX(DATETIME) AS 시간
+FROM ANIMAL_INS;
+
+/*최솟값 구하기*/
+SELECT MIN(DATETIME) AS 시간
+FROM ANIMAL_INS;
+
+/*동물 수 구하기*/
+SELECT COUNT(ANIMAL_ID)
+FROM ANIMAL_INS;
+
+/*중복 제거하기*/
+SELECT COUNT(DISTINCT NAME)
+FROM ANIMAL_INS
+WHERE NAME IS NOT NULL;
+/**/
+/**/
+/**/
+/**/
+/**/
+/**/
+/**/
+
+/**/
