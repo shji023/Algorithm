1b1f848596eb63809cb552cd2889ed4e599a20be
shji023 <shji0@naver.com>
2021-02-27 11:59:32 +0900

    21.02.10 SET

--- Baekjoon/1764 듣보잡.py
+++ Baekjoon/1764 듣보잡.py
@@ -1,8 +1,10 @@
 # set 교집합 이용
+
 N, M = map(int, input().split())
 d = set(list(input() for _ in range(N)))
 b = set(list(input() for _ in range(M)))
 db = sorted(d & b)
 print(len(db))
+
 for x in db:
     print(x)
