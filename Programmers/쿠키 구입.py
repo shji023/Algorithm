1ab7e7684326f381f469913fa3b45e16767a1bcd
shji023 <shji0@naver.com>
2021-03-05 14:13:55 +0900

    21.03.05 구현

--- Programmers/쿠키 구입.py
+++ Programmers/쿠키 구입.py
@@ -1,14 +1,15 @@
 import sys
+
 input = sys.stdin.readline
 
 
 def solution(cookie):
     answer = 0
     N = len(cookie)
-	# 반복문으로 인덱스값 하나씩 증가하면서
-    for idx in range(N-1):
+    # 반복문으로 인덱스값 하나씩 증가하면서
+    for idx in range(N - 1):
         # 현재 인덱스 기준으로 오른쪽 왼쪽 나눔
-        left, right = idx, idx+1
+        left, right = idx, idx + 1
         # 오른쪽 왼쪽 쿠키
         l_cookie, r_cookie = cookie[left], cookie[right]
         while left > -1 and right < N:
