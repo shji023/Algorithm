c0ce86bb299cf84b05d1de825683779fbf9cf199
JiyoungHyun <shyunji023@gmail.com>
2021-11-22 12:07:30 +0900

    21.11.22 구현

---
+++ Baekjoon/17413 단어 뒤집기2.py
@@ -0,0 +1,26 @@
+import sys
+
+sys.stdin = open("input.txt", 'r')
+input = sys.stdin.readline
+
+S = list(input())
+i = 0
+start = 0
+
+while i < len(S):
+    if S[i] == "<":
+        i += 1
+        while S[i] != ">":
+            i += 1
+        i += 1
+    elif S[i].isalnum():
+        start = i
+        while i < len(S) and S[i].isalnum():
+            i += 1
+        tmp = S[start:i]
+        tmp = tmp[::-1]
+        S[start:i] = tmp
+    else:
+        i += 1
+
+print("".join(S))
