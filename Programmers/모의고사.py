fa2dae4667cf5d064a33c9055dc953cf85497766
Jiyoung Hyun <60960130+shji023@users.noreply.github.com>
2022-01-31 20:52:02 +0900

    Update 모의고사.py

--- Programmers/모의고사.py
+++ Programmers/모의고사.py
@@ -15,3 +15,25 @@ def solution(answers):
         if max(answer_cnt) == answer_cnt[i]:
             result.append(i+1)
     return result
+
+def solution(answers):
+    answer = []
+    f_cnt, s_cnt, t_cnt = 0, 0, 0
+    first = [1, 2, 3, 4, 5]
+    second = [2, 1, 2, 3, 2, 4, 2, 5]
+    third = [3, 3, 1, 1, 2, 2, 4, 4, 5, 5]
+    for i in range(len(answers)):
+        if answers[i] == first[i%5]:
+            f_cnt += 1
+        if answers[i] == second[i%8]:
+            s_cnt += 1
+        if answers[i] == third[i%10]:
+            t_cnt += 1
+    Max = max(f_cnt, s_cnt, t_cnt)
+    if Max == f_cnt:
+        answer.append(1)
+    if Max == s_cnt:
+        answer.append(2)
+    if Max == t_cnt:
+        answer.append(3)
+    return answer
