a2368c9c2c7aabf60abfe87afcfe8ad873989a34
Jiyoung Hyun <60960130+shji023@users.noreply.github.com>
2022-02-04 16:48:24 +0900

    Create 두개 뽑아서 더하기.py
    문자열

---
+++ Programmers/두개 뽑아서 더하기.py
@@ -0,0 +1,10 @@
+def solution(numbers):
+    answer = []
+    temp = set()
+    for i in range(len(numbers)):
+        for j in range(i+1, len(numbers)):
+            temp.add(numbers[i]+numbers[j])
+    for i in temp:
+        answer.append(i)
+    answer.sort()
+    return answer
